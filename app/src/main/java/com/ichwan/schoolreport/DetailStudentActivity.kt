18d1e7e175cf0a5b415f0bb2cd085b1a595026de
ichwansh03 <ichwansholihin70@gmail.com>
2025-08-01 22:48:47 +0700

    init project

---
+++ app/src/main/java/com/ichwan/schoolreport/DetailStudentActivity.kt
@@ -0,0 +1,12 @@
+package com.ichwan.schoolreport
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class DetailStudentActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_student)
+
+    }
+}
