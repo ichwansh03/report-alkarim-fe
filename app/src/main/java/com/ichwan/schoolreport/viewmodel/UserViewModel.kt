0aec146b3eb49df7460d3de7c0ef879d80bcc3bb
ichwansh03 <ichwansholihin70@gmail.com>
2025-12-24 06:43:16 +0700

    add login to view model

--- app/src/main/java/com/ichwan/schoolreport/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/ichwan/schoolreport/viewmodel/UserViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.ichwan.schoolreport.model.LoginRequest
 import com.ichwan.schoolreport.model.User
 import com.ichwan.schoolreport.repository.UserRepository
 import kotlinx.coroutines.Dispatchers
@@ -19,14 +20,35 @@ class UserViewModel(private val repository: UserRepository) : ViewModel() {
     private val _message = MutableLiveData<String>()
     val message: LiveData<String> = _message
 
+    private val _loginSuccess = MutableLiveData<Boolean>()
+    val loginSuccess: LiveData<Boolean> = _loginSuccess
+
     fun registerUser(user: User) {
         viewModelScope.launch {
             try {
                 val response = repository.register(user)
                 if (response.isSuccessful) {
+                    _loginSuccess.value = true
                     _message.value = "User registered successfully"
                 } else {
+                    _loginSuccess.value = false
                     _message.value = "Failed to register user: ${response.message()}"
+                }
+            } catch (e: Exception) {
+                _loginSuccess.value = false
+                _message.value = "An error occurred: ${e.message}"
+            }
+        }
+    }
+
+    fun loginUser(loginRequest: LoginRequest) {
+        viewModelScope.launch {
+            try {
+                val response = repository.login(loginRequest)
+                if (response.isSuccessful) {
+                    _message.value = "Login successful"
+                } else {
+                    _message.value = "Login failed: ${response.message()}"
                 }
             } catch (e: Exception) {
                 _message.value = "An error occurred: ${e.message}"
